5a66e24ec00565e6d5034d245db6bdebea989c6f
Bowen Cheng <blueicycky@gmail.com>
2017-11-10 18:12:21 -0500

    Demonstrate a way of implementing iterator for a custom class

--- test/functional-programming/iterators.js
+++ test/functional-programming/iterators.js
@@ -33,4 +33,47 @@ describe('Iterables', function () {
     }
     assert.equal(sum, 6);
   });
+
+  it('can be built be implementing Symbol.iterator', function () {
+    class Company {
+      constructor() {
+        this.employees = [];
+      }
+
+      addEmployees(...names) {
+        this.employees = this.employees.concat(names);
+      }
+
+      [Symbol.iterator]() {
+        // Delegate the iterator to a generically reusable class
+        return new ArrayIterator(this.employees);
+      }
+    }
+
+    class ArrayIterator {
+      constructor(array) {
+        this.array = array;
+        this.index = 0;
+      }
+
+      next() {
+        let result = {value: undefined, done: true};
+        if (this.index < this.array.length) {
+          result.value = this.array[this.index];
+          result.done = false;
+          this.index+=1;
+        }
+        return result;
+      }
+    }
+
+    let count = 0;
+    let company = new Company();
+    company.addEmployees('Tim', 'Bob', 'Joy', 'Tom');
+
+    for (let employee of company) { // Using the iterator of Company class
+      count += 1;
+    }
+    assert.equal(count, 4);
+  });
 });
