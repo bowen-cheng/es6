d15286d53623995a9aed39b87a1f6f2c31d2f3d5
Bowen Cheng <blueicycky@gmail.com>
2017-11-20 07:13:48 -0500

    Combine generator and setTimeout function to solve ChristmasTree problem
    This also results in more readable code. The change actually mimics
    asynchronous behaviour using synchronous calls.

--- test/asynchronous/async-generator.js
+++ test/asynchronous/async-generator.js
@@ -4,15 +4,54 @@ const assert = require('assert');
 
 describe('Async generators', function () {
 
-  it('introduces christmas tree problem with regular callback', function () {
+  it('traditional callbacks introduce christmas tree problem', function () {
     setTimeout(function () {
-      console.log('1');
+      console.log('Christmas tree: 1');
       setTimeout(function () {
-        console.log('2');
+        console.log('Christmas tree: 2');
         setTimeout(function () {
-          console.log('3');
+          console.log('Christmas tree: 3');
         }, 1000);
       }, 1000);
     }, 1000);
+  });
+
+  it('should be easy to read with generators', function () {
+    let async;
+    (function(){
+      let sequence;
+      let run = function (generator) {
+        // make the generator function available at IIFE scope
+        sequence = generator();
+        //Call next to start
+        sequence.next();
+      };
+      let resume = function () {
+        sequence.next();
+      };
+      async = {
+        run: run,
+        resume: resume
+      };
+    }());
+
+    function pause(delay) {
+      setTimeout(function () {
+        console.log('Paused for ' + delay +' ms');
+        async.resume();
+      }, delay);
+    }
+
+    function* main() {
+      console.log('1');
+      yield pause(1000);
+      console.log('2');
+      yield pause(1000);
+      console.log('3');
+      yield pause(1000);
+      done();
+    }
+
+    async.run(main);
   });
 });
