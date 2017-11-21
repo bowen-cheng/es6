715c07a522512345703c01a078cdb911bda794bf
Bowen Cheng <blueicycky@gmail.com>
2017-11-21 08:06:11 -0500

    Let the generator funciton return some data

--- test/asynchronous/async-generator.js
+++ test/asynchronous/async-generator.js
@@ -2,6 +2,42 @@
 
 const assert = require('assert');
 
+// For test 2, 3
+let async;
+(function () {
+  let sequence;
+
+  const run = function (generator) {
+    // make the generator function available at IIFE scope
+    sequence = generator();
+    //Call next to start
+    sequence.next();
+  };
+
+  const resume = function (value) {
+    sequence.next(value);
+  };
+
+  async = {
+    run: run,
+    resume: resume,
+  };
+}());
+
+// For test 3
+function getPrice() {
+  setTimeout(function () {
+    async.resume(50);
+  }, 1000);
+}
+
+function doTrade() {
+  setTimeout(function () {
+    console.log('Deal!');
+    async.resume();
+  }, 1000);
+}
+
 describe('Async generators', function () {
 
   it('traditional callbacks introduce christmas tree problem', function () {
@@ -17,27 +53,9 @@ describe('Async generators', function () {
   });
 
   it('should be easy to read with generators', function () {
-    let async;
-    (function(){
-      let sequence;
-      let run = function (generator) {
-        // make the generator function available at IIFE scope
-        sequence = generator();
-        //Call next to start
-        sequence.next();
-      };
-      let resume = function () {
-        sequence.next();
-      };
-      async = {
-        run: run,
-        resume: resume
-      };
-    }());
-
     function pause(delay) {
       setTimeout(function () {
-        console.log('Paused for ' + delay +' ms');
+        console.log('Paused for ' + delay + ' ms');
         async.resume();
       }, delay);
     }
@@ -49,6 +67,20 @@ describe('Async generators', function () {
       yield pause(1000);
       console.log('3');
       yield pause(1000);
+      done();
+    }
+
+    async.run(main);
+  });
+
+  it('should work with returned data', function (done) {
+    function* main() {
+      let price = yield getPrice();
+      if (price < 60) {
+        doTrade();
+      } else {
+        console.log('No deal!');
+      }
       done();
     }
 
