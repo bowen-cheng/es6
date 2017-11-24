97a87511b0a88523197038f728a37ffeea95e83b
Bowen Cheng <blueicycky@gmail.com>
2017-11-24 08:03:22 -0500

    Demonstrate the usage of initializer of property and method

--- test/built-in-objects/object.js
+++ test/built-in-objects/object.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 
 describe('Object', function () {
-  describe('is(a, b) function', function () {
+  describe('is() function', function () {
     it('should consider +0 and -0 to be different', function () {
       assert.equal(-0 === 0, true); // There is 1 more bits for the minus sign
       assert.equal(Object.is(-0, 0), false); // correct
@@ -37,5 +37,40 @@ describe('Object', function () {
         assert.equal(ship.explode, true);
       });
     });
+
+    describe('property initializer shorthand', function () {
+      it('should create properties from local variables', function () {
+        const model = 'Ford';
+        const year = 1991;
+
+        // const Classic = {
+        //   model: model,
+        //   year: year
+        // };
+        // same as below
+        const Classic = {
+          model,
+          year
+        };
+
+        assert.equal(Classic.model, model);
+        assert.equal(Classic.year, year);
+      });
+    });
+
+    describe('method initializer shorthand', function () {
+      it('should create methods using shorthand', function () {
+        const server = {
+          // getPort: function () {
+          //   return 8080;
+          // }
+          // same as below
+          getPort() {
+            return 8080;
+          }
+        };
+        assert.equal(server.getPort(), 8080);
+      });
+    })
   });
 });
